d1f294daa6abe4f68c123d8fd03e6d54e6891949
khaledsahsah <khaled.sahsah@gmail.com>
2022-01-07 13:33:47 -0500

    secretkey

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module SubdomainRails
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts = nil
+    config.secret_key_base = '12345678'
   end
 end
