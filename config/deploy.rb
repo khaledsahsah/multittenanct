a6d1670522b13eb0ccd6a079421f88410a89d8fe
khaledsahsah <khaled.sahsah@gmail.com>
2022-01-08 21:19:22 -0500

    app name change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "testapp"
+set :application, "subdomain_rails"
 set :repo_url, "git@github.com:khaledsahsah/multittenanct.git"
 set :branch, 'main'
 
